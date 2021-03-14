5128e57cb1f29bf51c57d6c3422f32c59d37fe5c
Teodor Georgiev <56063596+tedogeorgiev@users.noreply.github.com>
2021-03-14 15:30:22 +0200

    Create main.cpp

---
+++ Travell App/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+
+#include "UI.h"
+
+
+using namespace std;
+
+
+
+int main()
+{
+    UI ui;
+    ui.menu();
+    return 0;
+}
